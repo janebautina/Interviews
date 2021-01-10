a278e6e600ac4edc66ac06e9cfc594dbd7286562
Evgeniya Bautina <evgeniya@Evgeniyas-MacBook-Pro.local>
2021-01-10 15:07:28 -0800

    Fibonacii Numbers

---
+++ FibonacciNumbersDP.java
@@ -0,0 +1,22 @@
+/*
+DP Solution for Fibonecci Numbers
+Time Complexity: O(n)
+Space: O(1)
+*/
+
+class FibonacciNumbersDP {
+  public int fib(int N) {
+    if (N <= 1) {
+        return N;
+    }
+    int prev = 1;
+    int prevPrev = 0;
+    int current = 0;
+    for (int i = 2; i <= N; i++) {
+      current = prev + prevPrev;
+      prevPrev = prev;
+      prev = current;
+    }
+    return current;
+  }
+}
