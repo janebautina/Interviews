cb376561130fd31fec0fd10cdbd48d3990939d60
Evgeniya Bautina <janebarinova@gmail.com>
2021-05-03 15:45:08 -0700

    Leetcode 104 Easy

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,8 @@
+class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+       return Math.max(maxDepth(root.right), maxDepth(root.left)) + 1;
+    }
+}
